9c519111d170c2b478ec6dd4f8978b4fcb8fcb85
Peter Gyorfi <hello@apgyorfi.dev>
2025-04-26 15:12:28 +0200

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/tudásbázis">
+            Irány a tudásbázis!
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Példakódok, sablonok és gyakran használt megoldásaim, tananyagaim gyűjteménye.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
